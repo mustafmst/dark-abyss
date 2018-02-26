ae0a1c5a3693b24c8c2751a928927031d6cc0678
Paweł Mstowski <pawel.mstowski@gmail.com>
2018-02-26 19:17:28 +0100

    Add some tiles and improve movements

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-const SPEED = 200
-const JUMP_FORCE = -300
+const SPEED = 10
+const MAX_SPEED = 150
+const JUMP_FORCE = -250
 const JUMP_CHANGE_DIRECTION_FORCE = 10
 const GRAVITY = 10
 const UP = Vector2(0, -1)
@@ -16,10 +17,15 @@ func _process(delta):
 		move_x(true)
 	elif is_on_floor():
 			motion.x = 0
-			$Sprite.play("idle")
 	if Input.is_action_just_pressed("ui_up") && is_on_floor():
 		motion.y = JUMP_FORCE
+	if !is_on_floor():
 		$Sprite.play("jump")
+	else :
+		if motion.x == 0 :
+			$Sprite.play("idle")
+		else :
+			$Sprite.play("run")
 	motion = move_and_slide(motion, UP)
 	if motion.x != 0:
 		$Sprite.flip_h = !motion.x > 0
@@ -34,12 +40,11 @@ func move_x(left):
 	if left:
 		direction = -1
 	if is_on_floor():
-		$Sprite.play("run")
-		motion.x = SPEED * direction
+		motion.x += SPEED * direction
 	else :
 		motion.x += direction*JUMP_CHANGE_DIRECTION_FORCE
-		if motion.x > SPEED:
-			motion.x = SPEED
-		elif motion.x < -SPEED:
-			motion.x = -SPEED
+	if motion.x > MAX_SPEED:
+		motion.x = MAX_SPEED
+	elif motion.x < -MAX_SPEED:
+		motion.x = -MAX_SPEED
 	pass
